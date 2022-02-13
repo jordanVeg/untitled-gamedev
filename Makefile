8d815ff1787993e74cd797d21fd05bb8daba09c1
Jordan Veg <jordanveg@gmail.com>
2022-02-12 19:17:23 -0500

    basics of player creation and movement

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@
 #LOAD_DIR := $(ALLEGRO_DIR)/bin
 #CFLAGS := -std=gnu99 -Wall -g -I$(INCLUDE_DIR) -Os
 #LDLIBS := -L$(LOAD_DIR) -lallegro -lallegro_main -lallegro_font -lallegro_image -lallegro_audio
-CFLAGS+=-std=gnu99 -Wall -g `pkg-config --cflags allegro-5 allegro_main-5 allegro_font-5 allegro_image-5 allegro_audio-5 allegro_primitives-5 allegro_dialog-5` -Os
-LDLIBS+=`pkg-config --libs allegro-5 allegro_main-5 allegro_font-5 allegro_image-5 allegro_audio-5 allegro_primitives-5 allegro_dialog-5`
+CFLAGS+=-std=gnu99 -Wall -g `pkg-config --cflags allegro-5 allegro_main-5 allegro_font-5 allegro_image-5 allegro_primitives-5` -Os
+LDLIBS+=`pkg-config --libs allegro-5 allegro_main-5 allegro_font-5 allegro_image-5 allegro_primitives-5`
 CC:=gcc
 all: main
 clean:
--- main.c
+++ main.c
@@ -4,30 +4,97 @@
 #include <stdlib.h>
 
 /* Allegro Libraries */
-#include <allegro5/allegro5.h>      /* Base Allegro Include */
-#include <allegro5/allegro_font.h>  /* Allegro Font library */
-#include <allegro5/allegro_image.h> /* Allegro Image library */
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro5.h>              /* Base Allegro library */
+#include <allegro5/allegro_font.h>          /* Allegro Font library */
+#include <allegro5/allegro_image.h>         /* Allegro Image library */
+#include <allegro5/allegro_primitives.h>    /* Allegro Primatives library */
 
 #define FPS 30.0
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
 
-int main(int argc, char** argv)
+#define PLAYER_WIDTH 64
+#define PLAYER_HEIGHT 64
+#define PLAYER_SPEED 64
+
+typedef struct player
 {
-    al_set_config_value(al_get_system_config(), "trace", "level", "debug");
-    if(!al_init())
+    int pos_x;
+    int pos_y;
+    int width;
+    int height;
+    int vel_x;
+    int vel_y;
+    int speed;
+    ALLEGRO_BITMAP* sprite;
+/*
+    void (*initialize)(struct player* p);
+    void (*spawn)(int start_x, int start_y, struct player* p);
+    void (*update)(struct player* p);
+*/
+} Player;
+
+int spawn_player(int start_x, int start_y, struct player* p)
+{
+    p->pos_x = start_x;
+    p->pos_y = start_y;
+    p->width = PLAYER_WIDTH;
+    p->height = PLAYER_HEIGHT;
+    p->vel_x = 0;
+    p->vel_y = 0;
+    p->speed = PLAYER_SPEED;
+    p->sprite = al_load_bitmap("assets/wizard.png");
+    if(!p->sprite)
     {
-        printf("couldn't initialize allegro\n");
+        printf("Error loading player sprite!\n");
         return 1;
     }
 
-    if(!al_install_keyboard())
-    {
-        printf("couldn't initialize keyboard\n");
-        return 1;
+    return 0;
+}
+
+void update_player(ALLEGRO_KEYBOARD_EVENT keyboard, struct player* p)
+{
+    /* Update speed based on Button press */
+    if(keyboard.keycode ==  ALLEGRO_KEY_W){
+        //p->vel_y = -p->speed;
+        p->pos_y -= p->speed;
     }
+    if(keyboard.keycode ==  ALLEGRO_KEY_S){
+        //p->vel_y = p->speed;
+        p->pos_y += p->speed;
+    }
+    if(keyboard.keycode ==  ALLEGRO_KEY_A){
+        //p->vel_x = -p->speed;
+        p->pos_x -= p->speed;
+    }
+    if(keyboard.keycode ==  ALLEGRO_KEY_D){
+        //p->vel_x = p->speed;
+        p->pos_x += p->speed;
+    }
+    /* Update position based on speed */
+    //p->pos_x += p->vel_x;
+    //p->pos_y += p->vel_y;
+
+    //p->vel_x = 0;
+    //p->vel_y = 0;
+}
+
+void show_player(struct player* p)
+{
+    /* Draw player */
+    al_draw_bitmap(p->sprite, p->pos_x, p->pos_y, 0 );
+}
+
+int main(int argc, char** argv)
+{
+//    al_set_config_value(al_get_system_config(), "trace", "level", "debug");
+
+    /* Initialize everything */
+    assert(al_init());                  /* Allegro environment */
+    assert(al_install_keyboard());      /* Install Keyboard */
+    assert(al_init_primitives_addon()); /* Initialize Primatives */
+    assert(al_init_image_addon());      /* Initialize Image Library */
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);
     if(!timer)
@@ -41,19 +108,7 @@ int main(int argc, char** argv)
     {
         printf("couldn't initialize queue\n");
         return 1;
-    }
-    al_init_primitives_addon();
-    if(!al_install_audio())
-    {
-        printf("couldn't initialize audio\n");
-        return 1;
-    }
-    
-    if(!al_init_image_addon())
-    {
-        printf("couldn't initialize image addon.\n");
-        return 1;
-    }
+    }    
 
     ALLEGRO_BITMAP* forest = al_load_bitmap("assets/Forest.jpg");
     if(!forest)
@@ -79,7 +134,17 @@ int main(int argc, char** argv)
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+    /* Player Setup (hopefully) */
+    Player p;
+    int status = spawn_player(SCREEN_WIDTH/2 - PLAYER_WIDTH/2, SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2, &p);
 
+    if(status != 0)
+    {
+        printf("an Error has occured. Exiting...");
+        return 1;
+    }
+
+    /* Game Loop */
     bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
@@ -96,7 +161,14 @@ int main(int argc, char** argv)
                 redraw = true;
                 break;
 
-            case ALLEGRO_EVENT_KEY_DOWN:
+            case ALLEGRO_EVENT_KEY_CHAR:
+                if(event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+                {
+                    done = true;
+                    break;
+                }
+                update_player(event.keyboard, &p);
+                break;
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
                 done = true;
                 break;
@@ -107,16 +179,8 @@ int main(int argc, char** argv)
 
         if(redraw && al_is_event_queue_empty(queue))
         {
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
-            al_draw_bitmap(forest, 0, 0, 0 );
-
-            al_draw_filled_triangle(35, 350, 85, 375, 35, 400, al_map_rgb_f(0, 1, 0));
-            al_draw_filled_rectangle(240, 260, 340, 340, al_map_rgba_f(0, 0, 0.5, 0.5));
-            al_draw_circle(450, 370, 30, al_map_rgb_f(1, 0, 1), 2);
-            al_draw_line(440, 110, 460, 210, al_map_rgb_f(1, 0, 0), 1);
-            al_draw_line(500, 220, 570, 200, al_map_rgb_f(1, 1, 0), 1);
-
+            al_draw_bitmap(forest, 0, 0, 0);
+            show_player(&p);
             al_flip_display();
 
             redraw = false;
@@ -124,6 +188,7 @@ int main(int argc, char** argv)
     }
 
     al_destroy_bitmap(forest);
+    al_destroy_bitmap(p.sprite);
     al_destroy_font(font);
     al_destroy_display(disp);
     al_destroy_timer(timer);
